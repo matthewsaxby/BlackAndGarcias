8a3ab6537df56003327548a285ec5dc1d3d76c8b
lachd <lachlan@ubuntu.(none)>
2012-09-26 22:29:56 +1000

    removed beans to reduce confusion. Use DTOs

--- src/edu/unsw/comp9321/beans/Actor.java
+++
@@ -1,9 +0,0 @@
-package edu.unsw.comp9321.beans;
-
-import java.util.List;
-
-public class Actor {
-	private List<Movie> moviesBeenIn;
-	private String firstName;
-	private String lastName;
-}
--- src/edu/unsw/comp9321/beans/SessionBean.java
+++
@@ -1,5 +0,0 @@
-package edu.unsw.comp9321.beans;
-
-public class SessionBean {
-
-}
