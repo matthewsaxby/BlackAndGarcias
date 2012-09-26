f7be1f8df9ea9d431f80f5cbb2a1a851283b5fba
lachd <lachlan@ubuntu.(none)>
2012-09-26 22:31:08 +1000

    readded session bean lol

---
+++ src/edu/unsw/comp9321/beans/SessionBean.java
@@ -0,0 +1,13 @@
+package edu.unsw.comp9321.beans;
+
+public class SessionBean {
+	private int userType;
+
+	public int getUserType() {
+		return userType;
+	}
+
+	public void setUserType(int userType) {
+		this.userType = userType;
+	}
+}
